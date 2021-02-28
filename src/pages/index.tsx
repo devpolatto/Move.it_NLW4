32d689887ae64a40e2a932e0b6025a893fe46202
devpolatto <polatto.xcode.job@gmail.com>
2021-02-27 22:48:26 -0300

    geting data in serverSideRendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { GetServerSideProps } from 'next';
 
 import { CountdownProvider } from '../contexts/CountdownContext';
 
@@ -8,7 +9,10 @@ import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
 import { ChallengeBox } from '../components/ChallengeBox';
 
-export default function Home() {
+export default function Home(props) {
+
+  console.log(props)
+
   return (
     <div className="containerApp">
       <Head><title>Inicio | move.it</title></Head>
@@ -29,4 +33,17 @@ export default function Home() {
       
     </div>
   );
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  
+  const { level, currentExperience, challengeCompleted } = ctx.req.cookies;
+
+  return{
+    props: {
+      level,
+      currentExperience,
+      challengeCompleted,
+    }
+  };
 }
